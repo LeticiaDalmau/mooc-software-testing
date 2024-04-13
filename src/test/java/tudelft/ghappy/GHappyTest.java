a8076962d46f8095c9dca0d7a08dc0d6e77747b9
LeticiaDalmau <leticiadalmaugonzalez@gmail.com>
2024-04-12 22:32:07 -0400

    better of GHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,15 +1,20 @@
 package tudelft.ghappy;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class GHappyTest {
+    private GHappy str;
+    @BeforeEach
+    public void initialize() {
+        this.str = new GHappy();
+    }
+
     @ParameterizedTest
     @CsvSource( {"xxggxx", "ggxx", "xgg"})
     public void gHappy(String g) {
-        GHappy str = new GHappy();
         boolean result = str.gHappy(g);
         Assertions.assertTrue(result);
     }
@@ -17,7 +22,6 @@ public class GHappyTest {
     @ParameterizedTest
     @CsvSource( {"xxgxx", "xxggyygxx", "gxgx", "xgxg"})
     public void gUnHappy(String g) {
-        GHappy str = new GHappy();
         boolean result = str.gHappy(g);
         Assertions.assertFalse(result);
     }
@@ -25,7 +29,6 @@ public class GHappyTest {
     @ParameterizedTest
     @CsvSource( {"xxGGxx", "xxGxx", "xxGGyygxx"})
     public void gHappyLowerCase(String g) {
-        GHappy str = new GHappy();
         boolean result = str.gHappy(g);
         Assertions.assertFalse(result);
     }
@@ -33,7 +36,6 @@ public class GHappyTest {
     @ParameterizedTest
     @CsvSource( {"xxiixx", "xxixx", "xxyyxx"})
     public void gHappyNoExistG(String g) {
-        GHappy str = new GHappy();
         boolean result = str.gHappy(g);
         Assertions.assertFalse(result);
     }
