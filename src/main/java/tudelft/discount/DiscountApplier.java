cb9c5635361bca3c2558ace1e8d5c7586d9c8094
LeticiaDalmau <leticiadalmaugonzalez@gmail.com>
2024-04-18 21:39:09 -0400

    Practical exercise 2  DiscountApplier

--- src/main/java/tudelft/discount/DiscountApplier.java
+++ src/main/java/tudelft/discount/DiscountApplier.java
@@ -1,5 +1,10 @@
 package tudelft.discount;
 
+import tudelft.invoice.Invoice;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiscountApplier {
 
     private ProductDao dao;
@@ -8,16 +13,22 @@ public class DiscountApplier {
         this.dao = dao;
     }
 
-    public void setNewPrices() {
+    public List<Product> setNewPrices() {
+
+        List<Product> all = dao.all();
+        List<Product> prices = new ArrayList<>();
 
         for(Product product : dao.all()) {
             if(product.getCategory().equals("BUSINESS")) {
                 product.setPrice(product.getPrice() * 0.9);
+                prices.add(product);
             }
             if(product.getCategory().equals("HOME")) {
                 product.setPrice(product.getPrice() * 1.1);
+                prices.add(product);
             }
         }
 
+        return prices;
     }
 }
--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,31 @@
 package tudelft.discount;
 
-public class DiscountApplierTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.util.Arrays;
+import java.util.List;
+
+public class DiscountApplierTest {
+    @Test
+    void discountApplier(){
+
+        Product table = new Product("Table",1, "HOME");
+        Product pc = new Product("PC", 1, "BUSINESS");
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+
+        List<Product> list = Arrays.asList(table, pc);
+
+        Mockito.when(dao.all()).thenReturn(list);
+
+        DiscountApplier NewPrices = new DiscountApplier(dao);
+        List<Product> result = NewPrices.setNewPrices();
+
+        Assertions.assertEquals(0.9, result.get(1).getPrice());
+        Assertions.assertEquals(2, result.size());
+
+        dao.close();
+    }
 }
